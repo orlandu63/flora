7f826cddc67fa62442428091095f9c89430d5f2a
Patrick Palka <orlandu96@gmail.com>
2009-03-22 11:56:33 -0400

    made post.php 10x better

--- post.php
+++ post.php
@@ -20,40 +20,43 @@ try {
 			$title = Input::validate(INPUT::VALIDATE_TITLE);
 		}
 	}
+	
+	$valid = true;
 } catch(Exception $exception) {
-	#header('HTTP/1.1 400 Bad Request');
 	echo '<p id="error">',  $exception->getMessage();
 	if($exception instanceof LengthException) {
 		echo '<br/>Note that "&lt;," "&gt;" and "&amp;" are actually 4, 4 and 5 characters in web form, respectively.';
 	}
 	echo '</p>';
-	return;
+	$valid = false;
 }
 
-if($replying_to) {
-	$topic_info = Topics::getInfo(Posts::getTopicFromId($replying_to));
-	echo '<h3>Replying to: <a href="', Page::PAGE_TOPIC, '?id=', $topic_info['id'], '">',
-		$topic_info['title'],
-	'</a></h3>';
-	Posts::display($replying_to);
-}
-
-if($preview) {
-	echo '<h3>Preview:';
-	if(!$replying_to) {
-		echo ' ', $title;
-	}
-	echo '</h3>';
-	Posts::display(array('body' => $body, 'author' => $author, 'toc' => time()));
-} elseif($submit) {
+if($valid) {
 	if($replying_to) {
-		$new_info = Posts::make($replying_to, $author, $body);
-	} else {
-		$new_info = Topics::make($title, $author, $body);
+		$topic_info = Topics::getInfo(Posts::getTopicFromId($replying_to));
+		echo '<h3>Replying to: <a href="', Page::PAGE_TOPIC, '?id=', $topic_info['id'], '">',
+			$topic_info['title'],
+		'</a></h3>';
+		Posts::display($replying_to);
 	}
-	header('HTTP/1.1 303 See Other');
-	header('Location: ' . Page::PAGE_TOPIC . '?id=' . $new_info[($replying_to ? 'topic' : 'id')]);
-	die;
+
+	if($preview) {
+		echo '<h3>Preview:';
+		if(!$replying_to) {
+			echo ' ', $title;
+		}
+		echo '</h3>';
+		Posts::display(array('body' => $body, 'author' => $author, 'toc' => time()));
+	} elseif($submit) {
+		if($replying_to) {
+			$new_info = Posts::make($replying_to, $author, $body);
+		} else {
+			$new_info = Topics::make($title, $author, $body);
+		}
+		header('HTTP/1.1 303 See Other');
+		header('Location: ' . Page::PAGE_TOPIC . '?id=' . $new_info[($replying_to ? 'topic' : 'id')]);
+		return;
+	}
 }
 
 $Page->title .= ' ' . ($replying_to ? 'Thread' : 'Topic');
