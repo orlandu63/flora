e2d4494baac1476ef0de2e3c67cdf933787e6c7a
Patrick Palka <orlandu96@gmail.com>
2009-07-09 23:07:16 -0400

    tidy up integer validating

--- index.php
+++ index.php
@@ -5,14 +5,16 @@ require CLASS_DIR . 'topiclist.phps';
 $Page->page_id = Page::PAGE_INDEX;
 $Page->site_nav['Home'] = '/';
 $Page->site_nav['Create a Topic'] = Page::makeURI(Page::PAGE_POST);
+$Page->header = 'Topic Index';
 
-$page_number = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-if($page_number < 0) {
+$page_number = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array('options' =>
+	array('min_range' => 0, 'max_range' => Topics::count() / Topiclist::PER_PAGE)
+));
+if($page_number === false) {
 	Page::error('Invalid page number', 400);
 	return;
 }
 
-$Page->header = 'Topic Index';
 if($page_number > 0) {
 	$Page->header .= sprintf(', page %d', $page_number + 1);
 }
